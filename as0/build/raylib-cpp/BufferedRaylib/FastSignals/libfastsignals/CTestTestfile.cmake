ff1dab0020f8eff041128cb1000b549e42baee27
Xue-Shangqi <112657781+Xue-Shangqi@users.noreply.github.com>
2024-02-02 23:47:52 -0800

    Delete as0/build/raylib-cpp directory
    should be ignored

--- as0/build/raylib-cpp/BufferedRaylib/FastSignals/libfastsignals/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/Documents/CS-381/as0/raylib-cpp/BufferedRaylib/FastSignals/libfastsignals
-# Build directory: /home/student/Documents/CS-381/as0/build/raylib-cpp/BufferedRaylib/FastSignals/libfastsignals
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
