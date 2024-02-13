754488e8145b82d3058cbb9647a6f32d71d6394d
shangqi <sxue@nevada.unr.edu>
2024-02-12 17:49:54 -0800

    Fixed loading mutliple models and updated compile instructions

--- as1/README.md
+++ as1/README.md
@@ -18,6 +18,10 @@ cd as1
 mkdir build
 cd build
 ```
+Once you have change directories into the build directory, copy and paste the customModel folder into the build directory or use the following command in the build directory.
+```
+cp -r ../customModel ./customModel
+```
 After the folder is made, run cmake and make and finally run the code:
 ```
 cmake ..
--- as1/src/as1.cpp
+++ as1/src/as1.cpp
@@ -1,54 +1,36 @@
 #include "raylib-cpp.hpp"
 
-//lambda function for translation
-auto translateModel = [](raylib::Model& model, float x, float y, float z){
-    model.transform = raylib::Transform(model.transform).Translate(x,y,z);
-};
-
-//function that is used to initialize and setup all the models so code looks cleaner
-void setUpModel(
-    raylib::Model& model, 
-    float x, float y, float z, 
-    float scaleX, float scaleY, float scaleZ, 
-    float degreeX, float degreeY, float degreeZ){
-    translateModel(model, x, y ,z);
-    model.transform = raylib::Transform(model.transform).Scale(scaleX, scaleY, scaleZ);
-    model.transform = raylib::Transform(model.transform).RotateXYZ(raylib::Degree(degreeX), raylib::Degree(degreeY), raylib::Degree(degreeZ));
+//Function to draw and translate the models
+void DrawBoundedModel(raylib::Model& model, Vector3 position, Vector3 scale, Vector3 rotation){
+    raylib::Transform ogTransform = model.transform;
+    model.transform = raylib::Transform(model.transform).Translate(position.x,position.y,position.z);
+    model.transform = raylib::Transform(model.transform).Scale(scale.x, scale.y, scale.z);
+    model.transform = raylib::Transform(model.transform).RotateXYZ(raylib::Degree(rotation.x), raylib::Degree(rotation.y), raylib::Degree(rotation.z));
+    model.Draw({});
+    model.GetTransformedBoundingBox().Draw();
+    model.transform = ogTransform;
 }
-
 
 int main(){
     //Initialize window 
     SetTargetFPS(60);
     InitWindow(800, 450, "CS381 - Assignment 1");
 
-    //Setup poly plane default
-    raylib::Model planeDefault = LoadModel("../customModel/PolyPlane.glb");
-    setUpModel(planeDefault, 100, 0, 0, 3, 3, 3, 0, 0, 0);
+    //Load ship
+    raylib::Model ship = LoadModel("customModel/ddg51.glb");
+    ship.transform = raylib::Transform(ship.transform).RotateXYZ(raylib::Degree(90), 0, raylib::Degree(90));
 
-    //Setup poly plane with changes
-    raylib::Model planeChanged = LoadModel("../customModel/PolyPlane.glb");
-    setUpModel(planeChanged, -100, 100, 0, 3, -3, 3, 0, 180, 0);
+    //Load plane
+    raylib::Model plane = LoadModel("customModel/PolyPlane.glb");
+    plane.transform = raylib::Transform(plane.transform).Scale(3, 3, 3);
 
-    //Setup ship one (left ship) 
-    raylib::Model shipOne = LoadModel("../customModel/ddg51.glb");
-    setUpModel(shipOne, -350, 0, 0, 1, 1, 1, 90, 0, 90);
+    //load bigBird 
+    raylib::Model phoenix = LoadModel("customModel/phoenix_bird.glb");
 
-    //Setup ship two (rightmost ship) 
-    raylib::Model shipTwo = LoadModel("../customModel/ddg51.glb");
-    setUpModel(shipTwo, 350, 0, 0, 1, 1, 1, 90, 0, 0);
+    //load skeleton 
+    raylib::Model head = LoadModel("customModel/head.glb");
+    head.transform = raylib::Transform(head.transform).RotateXYZ(raylib::Degree(90), raylib::Degree(180), 0);
 
-    //Setup ship two (middle-ish ship) 
-    raylib::Model shipThree = LoadModel("../customModel/ddg51.glb");
-    setUpModel(shipThree, 150, 150, 0, 1, 2, 1, 270, 180, 0);
-
-    //Setup bigBird 
-    raylib::Model phoenix = LoadModel("../customModel/phoenix_bird.glb");
-    setUpModel(phoenix, 0, -200, 0, .2, .2, .2, 0, 0, 0);
-
-    //Setup skeleton 
-    raylib::Model head = LoadModel("../customModel/head.glb");
-    setUpModel(head, 0, -50, 0, 50, 50, 50, -90, 0, 0);
 
     raylib::Camera camera(
         raylib::Vector3(0, 100, 800),
@@ -58,56 +40,65 @@ int main(){
         CAMERA_PERSPECTIVE
     );
 
-    float rotation = 0.0f;
+    //Plane animation variables
+    float planeOnePos = 100;
+    float planeOneRot = 0;
+    float planeTwoPos = -100;
+    float planeTwoRot = 0;
+
+    //Ship animation variables
+    float shipOneRot = 0;
+    float shipTwoRot = 90;
+    float shipThreeRot = 270;
+
+    //Head animation variables
+    float headRot = 0;
+
+    //Phoenix animation variables
+    float birdRot = 0;
 
     while (!WindowShouldClose()) {
+        //Plane animation 
+        planeOnePos += 50 * GetFrameTime();
+        planeOneRot += 30 * GetFrameTime();
+        planeTwoPos -= 50 * GetFrameTime();
+        planeTwoRot += 30 * GetFrameTime();
+
+        //Ship animation  
+        shipOneRot += 40 * GetFrameTime();
+        shipTwoRot -= 40 * GetFrameTime();
+        shipThreeRot += 40 * GetFrameTime();
+
+        //Head animation
+        headRot += 70 * GetFrameTime();
+
+        //Phoenix animation
+        birdRot += 55 * GetFrameTime();
+
         BeginDrawing();
         {   
             camera.BeginMode();
-            {   
-                //Animation for models
-                planeDefault.transform = raylib::Transform(planeDefault.transform).RotateXYZ(2*GetFrameTime(),0,0);
-                translateModel(planeDefault, 2, 0, 0);
-                planeChanged.transform = raylib::Transform(planeChanged.transform).RotateXYZ(-2*GetFrameTime(),0,0);
-                translateModel(planeChanged, -2, 0, 0);
-                shipOne.transform = raylib::Transform(shipOne.transform).RotateXYZ(0,2*GetFrameTime(),0);
-                shipTwo.transform = raylib::Transform(shipTwo.transform).RotateXYZ(0,-2*GetFrameTime(),0);
-                shipThree.transform = raylib::Transform(shipThree.transform).RotateXYZ(0,2*GetFrameTime(),0);
-                phoenix.transform = raylib::Transform(phoenix.transform).RotateXYZ(0,2*GetFrameTime(),0);
-                head.transform = raylib::Transform(head.transform).RotateXYZ(0,2*GetFrameTime(),0);
-
+            {  
                 //Clears the blackground so old drawing dont stack
                 ClearBackground(BLACK);
 
                 //Draws the models
-                DrawModel(planeDefault, raylib::Vector3(0,0,0), 1, WHITE);
-                DrawModel(planeChanged, raylib::Vector3(0,0,0), 1, WHITE);
-                DrawModel(shipOne, raylib::Vector3(0,0,0), 1, WHITE);
-                DrawModel(shipTwo, raylib::Vector3(0,0,0), 1, WHITE);
-                DrawModel(shipThree, raylib::Vector3(0,0,0), 1, WHITE);
-                DrawModel(phoenix, raylib::Vector3(0,0,0), 1, WHITE);
-                DrawModel(head, raylib::Vector3(0,0,0), 1, WHITE);
+                DrawBoundedModel(plane, {planeOnePos, 0, 0}, {1, 1, 1}, {planeOneRot, 0, 0});
+                DrawBoundedModel(plane, {planeTwoPos, 100, 0}, {1, -1, 1}, {planeTwoRot, 180, 0});
 
-                //Draws the bounding boxes
-                DrawBoundingBox(planeDefault.GetTransformedBoundingBox(), WHITE);
-                DrawBoundingBox(planeChanged.GetTransformedBoundingBox(), WHITE);
-                DrawBoundingBox(shipOne.GetTransformedBoundingBox(), WHITE);
-                DrawBoundingBox(shipTwo.GetTransformedBoundingBox(), WHITE);
-                DrawBoundingBox(shipThree.GetTransformedBoundingBox(), WHITE);
-                DrawBoundingBox(phoenix.GetTransformedBoundingBox(), WHITE);
-                DrawBoundingBox(head.GetTransformedBoundingBox(), WHITE);
-
+                DrawBoundedModel(ship, {-350, 0, 0}, {1, 1, 1}, {0, shipOneRot, 0});
+                DrawBoundedModel(ship, {350, 0, 0}, {1, 1, 1}, {0, shipTwoRot, 0});
+                DrawBoundedModel(ship, {150, 150, 0}, {1, 2, 1}, {0, shipThreeRot, 0});
+                DrawBoundedModel(phoenix, {0, -200, 0}, {.2, .2, .2}, {0, birdRot, 0});
+                DrawBoundedModel(head, {0, -50, 0}, {50, 50, 50}, {0, headRot, 0});
             }
             camera.EndMode(); 
         }
         EndDrawing();
     }
     //Unload all model in the scene 
-    UnloadModel(planeDefault); 
-    UnloadModel(planeChanged); 
-    UnloadModel(shipOne); 
-    UnloadModel(shipTwo); 
-    UnloadModel(shipThree); 
+    UnloadModel(plane); 
+    UnloadModel(ship); 
     UnloadModel(phoenix); 
     UnloadModel(head);
 }
