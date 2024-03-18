e4d63316537adfce19be513bcbed34ee96d06f26
shangqi <sxue@nevada.unr.edu>
2024-03-17 19:25:12 -0700

    Finished as6

--- as6/src/as6.cpp
+++ as6/src/as6.cpp
@@ -11,24 +11,30 @@ raylib::Transform transformer(const raylib::Transform& inputTransform) {
 }
 
 static constexpr auto AngleClamp = [](raylib::Degree angle) -> raylib::Degree {
-    int intPart = angle;
-    float floatPart = float(angle) - intPart;
-    intPart %= 360;
-    intPart += (intPart < 0) * 360;
-    return intPart + floatPart;
+		float decimal = float(angle) - int(angle);
+		int whole = int(angle) % 360;
+		whole += (whole < 0) * 360;
+		return decimal + whole;
 };
 
+
 struct CalculateData {
-    //Variables for position and speed 
+    // Variables for position and speed 
     float speed = 0;
     float speedTarget = 0;
-    float acceleration = 100;
-    raylib::Degree heading = 10;
-    raylib::Degree headingTarget = 10;
+    float acceleration = 50;
+    float turningRate = 70;
+	float maxSpeed = 90;
+    raylib::Degree heading = 0;
+    raylib::Degree headingTarget = 0;
     raylib::Degree elevation = 0;
     raylib::Degree elevationTarget = 0;
-    raylib::Vector3 velocity = {speed * cos(heading.RadianValue()), 0, -speed * sin(heading.RadianValue())};
-    raylib::Vector3 pos = {0, 0, 0};
+    raylib::Vector3 velocity {
+        speed * cos(heading.RadianValue()),
+        0,  // Assuming y-axis is vertical
+        -speed * sin(heading.RadianValue())
+    };
+    raylib::Vector3 pos {0, 0, 0};
 };
 
 using Entity = uint8_t;
@@ -134,41 +140,127 @@ struct Rendering {
 	bool drawBoundingBox = false;
 };
 
-struct InputComponent {
-	raylib::BufferedInput inputs;
-	CalculateData data;
-	InputComponent() : inputs(raylib::BufferedInput{}), data(CalculateData{}) {
+void Draw(Scene& scene);
+void createModel(Entity& entity, raylib::Model& model, Scene& scene, raylib::Vector3 pos);
+void Update(Scene& scene, raylib::BufferedInput& inputs, int selected);
+void ModifyData(Entity& entity, Scene& scene, float maxSpeed, float acceleration, float turnRate);
 
+// Input component that takes care of user inputs using a buffered input system
+struct InputComponent {
+	void PollEvent(raylib::BufferedInput& inputs){
+		inputs.PollEvents();
+	}
+
+	void ControlMovement(raylib::BufferedInput& inputs, CalculateData& data){
+
+		// The if state in each lambda function checks if the key is only pressed one
+		// because the code would run multiple times if the statement is not there 
+		// since it runs per frame
 		inputs["w"] = raylib::Action::key(KEY_W).SetPressedCallback([&]{
-			data.speedTarget += 20;
+			if(IsKeyPressed(KEY_W)){
+				data.speedTarget += 20;
+			}
 		}).move();
 
 		inputs["s"] = raylib::Action::key(KEY_S).SetPressedCallback([&]{
-			data.speedTarget -= 20;
+			if(IsKeyPressed(KEY_S)){
+				data.speedTarget -= 20;
+			}
 		}).move();
-    }
 
-	void PollEvents(){
-		inputs.PollEvents();
+		inputs["a"] = raylib::Action::key(KEY_A).SetPressedCallback([&]{
+			if(IsKeyPressed(KEY_A)){
+				data.headingTarget += 30;
+			}
+		}).move();
+
+		inputs["d"] = raylib::Action::key(KEY_D).SetPressedCallback([&]{
+			if(IsKeyPressed(KEY_D)){
+				data.headingTarget -= 30;
+			}
+		}).move();
+
+		inputs["space"] = raylib::Action::key(KEY_SPACE).SetPressedCallback([&]{
+			if(IsKeyPressed(KEY_SPACE)){
+				data.speedTarget = 0;
+			}
+		}).move();
 	}
 };
 
-void movementControl(Entity& entity, Scene& scene);
-void movementCalc(Entity& entity, Scene& scene);
-void Draw(Scene& scene);
-void createModel(Entity& entity, raylib::Model& model, Scene& scene, raylib::Vector3 pos);
+// Transform component storing the original transform when they are first created and
+// the transform used for calculation and rendering
+struct TransformComponent {
+	raylib::Transform ogTransform = raylib::Transform();
+	raylib::Transform newTransform = raylib::Transform();
+	TransformComponent() : ogTransform(), newTransform() {}
+};
+
+// Combined both phsyics and rendering component into one huge render component struct with all the 
+// physics calulation and setting the transform for rendering.
+struct RenderComponent {
+	void movementCalc(Entity& entity, Scene& scene) {
+		CalculateData& data = scene.GetComponent<CalculateData>(entity);
+		//Speed calculation
+		if(data.speedTarget > data.speed){
+			data.speed += data.acceleration * GetFrameTime();
+		}else if(data.speedTarget < data.speed || data.maxSpeed < data.speed){
+			data.speed -= data.acceleration * GetFrameTime();
+		}
+		data.velocity = raylib::Vector3{data.speed * cos(data.heading.RadianValue()), data.speed * sin(data.elevation.RadianValue()), -data.speed * sin(data.heading.RadianValue())};
+		data.pos += data.velocity * GetFrameTime();
+
+		//Heading calculation
+		// printf("Target: %f Heading: %f \n", (float)data.headingTarget, (float)data.heading);
+		data.headingTarget = AngleClamp(data.headingTarget);
+		data.heading = AngleClamp(data.heading);
+		float difference = abs(data.headingTarget - data.heading);
+		if(data.headingTarget > data.heading){
+			if(difference < 180) data.heading += data.turningRate * GetFrameTime();
+			else if(difference > 180) data.heading -= data.turningRate * GetFrameTime();
+		}else if(data.headingTarget < data.heading){
+			if(difference < 180) data.heading -= data.turningRate * GetFrameTime();
+			else if(difference > 180) data.heading += data.turningRate * GetFrameTime();
+		}
+
+		//Elevation calculation (did not use)
+		if(data.elevationTarget > data.elevation){
+			data.elevation += data.acceleration * GetFrameTime();
+		}else if(data.elevationTarget < data.elevation){
+			data.elevation -= data.acceleration * GetFrameTime();
+		}
+	}
+
+	void UpdateMovement(Entity& entity, Scene& scene, raylib::BufferedInput& inputs){
+		movementCalc(entity, scene);
+		InputComponent& inputComponent = scene.GetComponent<InputComponent>(entity);
+		CalculateData& data = scene.GetComponent<CalculateData>(entity);
+		Rendering& render = scene.GetComponent<Rendering>(entity);
+
+		inputComponent.PollEvent(inputs);
+		if(render.drawBoundingBox){
+			inputComponent.ControlMovement(inputs, data);
+		}
+
+		TransformComponent& transform = scene.GetComponent<TransformComponent>(entity);
+		transform.newTransform = transform.ogTransform.Translate(data.pos).RotateY(data.heading);
+	}
+};
+
+
 
 int main() {
 	// Create window
 	const int screenWidth = 800 * 1.5;
 	const int screenHeight = 450 * 1.5;
 	raylib::Window window(screenWidth, screenHeight, "CS381 - Assignment 6");
+	window.SetTargetFPS(60);
 
 	// Create camera
 	auto camera = raylib::Camera(
-		raylib::Vector3(0, 120, -500), 
-		raylib::Vector3(0, 0, 300), 
-		raylib::Vector3::Up(), 
+        raylib::Vector3(0, 200, -500),
+        raylib::Vector3(0, 0, 0),
+        raylib::Vector3::Up(),
 		45.0f,
 		CAMERA_PERSPECTIVE
 	);
@@ -184,8 +276,14 @@ int main() {
 	water.SetWrap(TEXTURE_WRAP_REPEAT);
 	ground.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = water;
 
-	// Create airplane
+	// Create audio 
+    InitAudioDevice();
+    Sound wind = LoadSound("sound/wind.mp3");
+
+	// Create plane
 	raylib::Model plane("customModel/PolyPlane.glb");
+	raylib::Model space("customModel/spaceship.glb");
+	space.transform = raylib::Transform(space.transform).Scale(4, 4, 4).RotateY(raylib::Degree(90));
 
 	// Create Ships
 	raylib::Model cargo("customModel/CargoG_HOSBrigadoon.glb");
@@ -218,7 +316,8 @@ int main() {
 	Entity sEntityFive = scene.CreateEntity();
 
 	// Creates the five planes
-	createModel(pEntityOne, plane, scene, {0, 80, 0});
+	createModel(pEntityOne, space, scene, {0, 80, 0});
+	ModifyData(pEntityOne, scene, 200.0, 100.0, 200.0);
 	createModel(pEntityTwo, plane, scene, {150, 80, 0});
 	createModel(pEntityThree, plane, scene, {300, 80, 0});
 	createModel(pEntityFour, plane, scene, {-150, 80, 0});
@@ -226,18 +325,52 @@ int main() {
 
 	// Creates the five ship
 	createModel(sEntityOne, cargo, scene, {-300, 0, 0});
+	ModifyData(sEntityOne, scene, 30.0, 20.0, 25.0);
 	createModel(sEntityTwo, ship, scene, {-150, 0, 0});
+	ModifyData(sEntityTwo, scene, 50.0, 40.0, 30.0);
 	createModel(sEntityThree, smit, scene, {0, 0, 0});
+	ModifyData(sEntityThree, scene, 40.0, 50.0, 35.0);
 	createModel(sEntityFour, explorer, scene, {150, 0, 0});
+	ModifyData(sEntityFour, scene, 60.0, 50.0, 40.0);
 	createModel(sEntityFive, oilTanker, scene, {300, 0, 0});
+	ModifyData(sEntityFive, scene, 35.0, 25.0, 30.0);
+
+	// Creates tracker for which plane is selected
+	int selected = 0;
 
 	// Main loop
 	bool keepRunning = true;
 	while(!window.ShouldClose() && keepRunning) {
-		// Updates
+		// Quit os escape is pressed
 		if(IsKeyPressed(KEY_ESCAPE)){
 			keepRunning = false;
 		}
+
+		// Selection system
+		if(IsKeyPressed(KEY_TAB)){
+			selected ++;
+			if (selected > 9) {
+				selected = 0;
+			}
+		}
+
+		// Sound system
+		CalculateData& dataOne = scene.GetComponent<CalculateData>(pEntityOne);
+		CalculateData& dataTwo = scene.GetComponent<CalculateData>(pEntityTwo);
+		CalculateData& dataThree = scene.GetComponent<CalculateData>(pEntityThree);
+		CalculateData& dataFour = scene.GetComponent<CalculateData>(pEntityFour);
+		CalculateData& dataFive = scene.GetComponent<CalculateData>(pEntityFive);
+		
+		if(dataOne.speed != 0 || dataTwo.speed != 0 || dataThree.speed != 0 || dataFour.speed != 0 || dataFive.speed != 0){
+			if (!IsSoundPlaying(wind)) {
+                PlaySound(wind);
+            }
+		} else {
+            if (IsSoundPlaying(wind)) {
+                StopSound(wind);
+            }
+        }
+
 		// Rendering
 		window.BeginDrawing();
 		{
@@ -250,11 +383,8 @@ int main() {
 				skybox.Draw();
 				ground.Draw({});
 
-				movementCalc(pEntityOne, scene);
-				InputComponent& inputComponent = scene.GetComponent<InputComponent>(pEntityOne);
-				inputComponent.PollEvents();
-				raylib::Transform& planeOne = scene.GetComponent<raylib::Transform>(pEntityOne);
-				planeOne = planeOne.Translate(inputComponent.data.pos).RotateY(raylib::Degree(inputComponent.data.heading));
+				
+				Update(scene, inputs, selected);
 				Draw(scene);
 			}
 			camera.EndMode();
@@ -265,61 +395,77 @@ int main() {
 		window.EndDrawing();
 	}
 
+	CloseAudioDevice();
 	return 0;
 }
 
-void movementCalc(Entity& entity, Scene& scene) {
-	InputComponent& inputComponent = scene.GetComponent<InputComponent>(entity);
-    //Speed calculation
-    if(inputComponent.data.speedTarget > inputComponent.data.speed){
-        inputComponent.data.speed += inputComponent.data.acceleration * GetFrameTime();
-    }else if(inputComponent.data.speedTarget < inputComponent.data.speed){
-        inputComponent.data.speed -= inputComponent.data.acceleration * GetFrameTime();
-    }
-    inputComponent.data.velocity = raylib::Vector3{-inputComponent.data.speed * cos(inputComponent.data.heading.RadianValue()), inputComponent.data.speed * sin(inputComponent.data.elevation.RadianValue()), inputComponent.data.speed * sin(inputComponent.data.heading.RadianValue())};
-    inputComponent.data.pos += inputComponent.data.velocity * GetFrameTime();
-    
-
-    //Heading calculation
-    inputComponent.data.headingTarget = AngleClamp(inputComponent.data.headingTarget);
-    inputComponent.data.heading = AngleClamp(inputComponent.data.heading);
-    float difference = abs(inputComponent.data.headingTarget - inputComponent.data.heading);
-    if(inputComponent.data.headingTarget > inputComponent.data.heading){
-        if(difference < 180) inputComponent.data.heading += inputComponent.data.acceleration * GetFrameTime();
-        else if(difference > 180) inputComponent.data.heading -= inputComponent.data.acceleration * GetFrameTime();
-    }else if(inputComponent.data.headingTarget < inputComponent.data.heading){
-        if(difference < 180) inputComponent.data.heading -= inputComponent.data.acceleration * GetFrameTime();
-        else if(difference > 180) inputComponent.data.heading += inputComponent.data.acceleration * GetFrameTime();
-    }
-
-    //Elevation calculation
-    if(inputComponent.data.elevationTarget > inputComponent.data.elevation){
-        inputComponent.data.elevation += inputComponent.data.acceleration * GetFrameTime();
-    }else if(inputComponent.data.elevationTarget < inputComponent.data.elevation){
-        inputComponent.data.elevation -= inputComponent.data.acceleration * GetFrameTime();
-    }
-}
-
+// Draws the whole scene (i.e. all models)
 void Draw(Scene& scene) {
 	for(Entity e = 0; e < scene.entityMasks.size(); e++){
 		if(!scene.HasComponent<Rendering>(e)) continue;
 		
 		auto& rendering = scene.GetComponent<Rendering>(e);
 
+		TransformComponent& transform = scene.GetComponent<TransformComponent>(e);
 		raylib::Transform backupTransform = rendering.model->transform;
-		rendering.model->transform = scene.GetComponent<raylib::Transform>(e);
+		rendering.model->transform = transform.newTransform;
 		rendering.model->Draw({});
-		rendering.model->transform = backupTransform;
+		if(rendering.drawBoundingBox){
+			rendering.model->GetTransformedBoundingBox().Draw();
 		}
+		rendering.model->transform = backupTransform;
+		transform.newTransform = transform.ogTransform;
+	}
 }
 
+// Updates the calcualtion for all models in the scene
+void Update(Scene& scene, raylib::BufferedInput& inputs, int selected){
+	for(Entity e = 0; e < scene.entityMasks.size(); e++){
+		if(!scene.HasComponent<RenderComponent>(e)) continue;
+	
+		RenderComponent& renderComponent = scene.GetComponent<RenderComponent>(e);
+		CalculateData& data = scene.GetComponent<CalculateData>(e);
+		Rendering& render = scene.GetComponent<Rendering>(e);
+
+		// Used to update the selected object
+		if(selected == e){
+			render.drawBoundingBox = true;
+		}else{
+			render.drawBoundingBox = false;
+		}
+
+
+		// Updates the movement and rotation
+		renderComponent.UpdateMovement(e, scene, inputs);
+	}
+}
+
+// Initial the model with all the components they need
 void createModel(Entity& entity, raylib::Model& model, Scene& scene, raylib::Vector3 pos){
 	Rendering& renderingComponent = scene.AddComponent<Rendering>(entity);
 	renderingComponent.model = &model;
-	raylib::Transform& transform = scene.AddComponent<raylib::Transform>(entity);
-	transform = renderingComponent.model->transform;
-	transform = transform.Translate(pos);
 
+	TransformComponent& transform = scene.AddComponent<TransformComponent>(entity);
+	transform.ogTransform = renderingComponent.model->transform;
+	transform.ogTransform = transform.ogTransform.Translate(pos);
 
+	// Create the input component to process inputs
 	InputComponent& inputComponent  = scene.AddComponent<InputComponent>(entity);
+
+	// Crates the data to use for movements
+	CalculateData& data = scene.AddComponent<CalculateData>(entity);
+	CalculateData holder = CalculateData();
+	data = holder;
+
+	// Creates the rendering component
+	RenderComponent& renderComponent = scene.AddComponent<RenderComponent>(entity);
+}
+
+// Modifies the data of different entity
+void ModifyData(Entity& entity, Scene& scene, float maxSpeed, float acceleration, float turnRate){
+	CalculateData& data = scene.GetComponent<CalculateData>(entity);
+
+	data.maxSpeed = maxSpeed;
+	data.acceleration = acceleration;
+	data.turningRate = turnRate;
 }
